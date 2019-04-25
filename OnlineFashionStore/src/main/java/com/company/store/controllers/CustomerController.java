8e5d0eb4f8468ee60a023de847fa95e918558d30
Yanjie He <heyanjie0@outlook.com>
2019-04-25 19:12:01 -0400

    front end

--- OnlineFashionStore/src/main/java/com/company/store/controllers/CustomerController.java
+++ OnlineFashionStore/src/main/java/com/company/store/controllers/CustomerController.java
@@ -1,4 +1,18 @@
 package com.company.store.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<Object> getAllCustomers() {
+        return new ResponseEntity<>("This is a test", HttpStatus.OK);
+    }
+
 }
