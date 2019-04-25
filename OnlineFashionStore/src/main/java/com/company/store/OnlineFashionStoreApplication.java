4684cff97b6b637dd68fcee9b1dc4353458abfa6
Yanjie He <heyanjie0@outlook.com>
2019-04-25 18:57:19 -0400

    customer controller

---
+++ OnlineFashionStore/src/main/java/com/company/store/OnlineFashionStoreApplication.java
@@ -0,0 +1,13 @@
+package com.company.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFashionStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFashionStoreApplication.class, args);
+	}
+
+}
---
+++ OnlineFashionStore/src/main/java/com/company/store/controllers/CustomerController.java
@@ -0,0 +1,4 @@
+package com.company.store.controllers;
+
+public class CustomerController {
+}
