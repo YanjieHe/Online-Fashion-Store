b80ab941f4a8febb0249ea70a605062ab54b639a
Yanjie He <heyanjie0@outlook.com>
2019-04-29 23:47:11 -0400

    product display restful api

--- online-fashion-store/src/ProductDisplay.js
+++ online-fashion-store/src/ProductDisplay.js
@@ -2,14 +2,26 @@ import React from 'react';
 import {Card, Button, Row, Col, Container} from "react-bootstrap";
 
 class ProductDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            product: {}
+        }
+    }
 
-    renderOneProduct(url) {
+    componentDidMount() {
+        fetch("http://localhost:8080/products/1")
+            .then(response => response.json())
+            .then(json => this.setState({product: json}));
+    }
+
+    renderOneProduct(title, price, imageUrl) {
         return <Card style={{width: '15rem'}}>
-            <Card.Img variant="top" src={url}/>
+            <Card.Img variant="top" src={imageUrl}/>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text></Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{price}</Card.Text>
+                {/*<Button variant="primary">Go somewhere</Button>*/}
             </Card.Body>
         </Card>;
     }
@@ -17,17 +29,7 @@ class ProductDisplay extends React.Component {
     render() {
         return <div>
             <Container>
-                <Row>
-                    <Col>
-                        {this.renderOneProduct("https://katespade.insnw.net/KateSpade/PXRUA160_399_1?$large$")}
-                    </Col>
-                    <Col>
-                        {this.renderOneProduct("https://katespade.insnw.net/KateSpade/PXRUA350_104_1?$large$")}
-                    </Col>
-                    <Col>
-                        {this.renderOneProduct("https://katespade.insnw.net/KateSpade/PXRUA350_429_1?$large$")}
-                    </Col>
-                </Row>
+                {this.renderOneProduct(this.state.product.product_name,this.state.product.price, this.state.product.image_link)}
             </Container>
         </div>
     }
