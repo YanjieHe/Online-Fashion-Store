02921b3554ad807aa0a7ee09c66236a5ef1f768d
LingmeiZhao <ZhaoLingmei0@outlook.com>
2019-05-27 14:23:41 -0400

    Add Shopping Cart

--- OnlineFashionStore/src/main/java/com/company/store/controllers/ShoppingCartController.java
+++ OnlineFashionStore/src/main/java/com/company/store/controllers/ShoppingCartController.java
@@ -6,9 +6,12 @@ import com.company.store.services.UserManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import com.company.store.services.UserManagementService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 @RestController
 public class ShoppingCartController {
@@ -41,4 +44,52 @@ public class ShoppingCartController {
             return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
         }
     }
+
+    public static class AddItem {
+        private Long sessionId;
+        private Integer inventoryId;
+        private Integer quantity;
+
+        public Long getSessionId() {
+            return sessionId;
+        }
+
+        public void setSessionId(Long sessionId) {
+            this.sessionId = sessionId;
+        }
+
+        public Integer getInventoryId() {
+            return inventoryId;
+        }
+
+        public void setInventoryId(Integer inventoryId) {
+            this.inventoryId = inventoryId;
+        }
+
+        public Integer getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(Integer quantity) {
+            this.quantity = quantity;
+        }
+    }
+
+    @RequestMapping(value = "/add_into_shopping_cart", method = RequestMethod.POST)
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<Object> addIntoShoppingCart(@RequestBody AddItem addItem ){
+        try{
+            Integer customerId = userManagementService.getCustomerId(addItem.getSessionId());
+            Integer inventoryId = addItem.getInventoryId();
+            Integer quantity = addItem.getQuantity();
+            Date date = new Date();
+            shoppingCartService.createShoppingCartItem(customerId, inventoryId, date, quantity);
+            return new ResponseEntity<>("Success",HttpStatus.OK);
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
 }
