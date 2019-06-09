6be830111a683002b15d316a337db38aee2c3bf7
Yanjie He <heyanjie0@outlook.com>
2019-06-09 18:13:00 -0400

    trending products

---
+++ online-fashion-store/src/TrendingProducts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from "./NavigationBar";
+
+class TrendingProducts extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavigationBar/>
+            </div>
+        );
+    }
+}
+
+export default TrendingProducts;
