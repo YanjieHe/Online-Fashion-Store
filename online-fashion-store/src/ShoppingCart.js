7cd15b09d6f232c4d766e38b8f67a878b3c23c2f
Yanjie He <heyanjie0@outlook.com>
2019-06-09 20:27:45 -0400

    shopping cart

--- online-fashion-store/src/ShoppingCart.js
+++ online-fashion-store/src/ShoppingCart.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {instanceOf} from "prop-types";
 import {Cookies, withCookies} from "react-cookie";
 import {withRouter} from 'react-router-dom';
-import {Image, Table} from "react-bootstrap";
+import {Image, Table, Button, ButtonToolbar} from "react-bootstrap";
 import NavigationBar from "./NavigationBar";
+import "./ShoppingCart.css";
 
 class ShoppingCart extends React.Component {
     static propTypes = {
@@ -75,27 +76,35 @@ class ShoppingCart extends React.Component {
         for (let i = 0; i < this.state.inventoryList.length; i++) {
             console.log(inventoryList[i]);
             rows.push(<tr>
-                <td><Image src={inventoryList[i].imageLink} width="100px"/></td>
-                <td>Handbag</td>
-                <td>{inventoryList[i].price}</td>
-                <td>{inventoryList[i].color}</td>
-                <td>{inventoryList[i].size}</td>
-                <td>{inventoryList[i].quantity}</td>
+                <td id="cart"><Image src={inventoryList[i].imageLink} width="50px"/></td>
+                <td id="cart">Handbag</td>
+                <td id="cart">${inventoryList[i].price.toFixed(2)}</td>
+                <td id="cart">{inventoryList[i].color}</td>
+                <td id="cart">{inventoryList[i].size}</td>
+                <td id="cart">{inventoryList[i].quantity}</td>
+                <td id="cart">
+                    <ButtonToolbar>
+                        <Button variant="primary">Move to Wish List</Button>
+                        <Button variant="danger">Remove</Button>
+                    </ButtonToolbar>
+                </td>
             </tr>)
         }
         return <div>
             <NavigationBar/>
-            <Table bordered hover>
+            <Table hover>
                 <thead>
-                <td>Product</td>
-                <td>Name</td>
-                <td>Price</td>
-                <td>Color</td>
-                <td>Size</td>
-                <td>Quantity</td>
+                <td id="cart">Product</td>
+                <td id="cart">Name</td>
+                <td id="cart">Price</td>
+                <td id="cart">Color</td>
+                <td id="cart">Size</td>
+                <td id="cart">Quantity</td>
+                <td id="cart"></td>
                 </thead>
                 <tbody>{rows}</tbody>
             </Table>
+            <Button variant="primary" onClick={() => this.props.history.push("/checkout")}>Proceed to Checkout</Button>
         </div>
     }
 }
