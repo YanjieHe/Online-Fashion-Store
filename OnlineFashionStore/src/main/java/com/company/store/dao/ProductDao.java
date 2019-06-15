b5fd7a8d0afa4961cddbd72039888702812b7164
Yanjie He <heyanjie0@outlook.com>
2019-06-15 15:44:21 -0400

    Update ProductDao.java

--- OnlineFashionStore/src/main/java/com/company/store/dao/ProductDao.java
+++ OnlineFashionStore/src/main/java/com/company/store/dao/ProductDao.java
@@ -6,13 +6,11 @@ import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.transform.AliasToEntityMapResultTransformer;
 import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
